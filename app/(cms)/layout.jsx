1c5ab1358f09f5aed21adbc2ecf5c2ef22016059
subhomoyprojects <subhomoyprojects@gmail.com>
2026-07-07 15:08:47 +0530

    design fix

--- app/(cms)/layout.jsx
+++ app/(cms)/layout.jsx
@@ -1,5 +1,20 @@
 import Navbar from "../../components/Navbar";
 import "./globals.css";
+import { Inter, Oswald } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-inter",
+  preload: true,
+});
+
+const oswald = Oswald({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-oswald",
+  preload: true,
+});
 
 export const metadata = {
   title: "Learn Next.js",
@@ -9,7 +24,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className={`${inter.variable} ${oswald.variable}`}>
         <div className="page-shell">
           <Navbar />
           {children}
